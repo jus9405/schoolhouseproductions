8631ce9247d77a560c9ef0a927175dbf4135eb6d
Tyler Romeo <tylerromeo+github@gmail.com>
2009-03-06 22:50:26 -0500

    Changing time zone in settings.py and setting db type of mysql.

--- schoolhouseproductions/settings.py
+++ schoolhouseproductions/settings.py
@@ -9,19 +9,19 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'                # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'schoolhouseproductions' # Or path to database file if using sqlite3.
+DATABASE_USER = ''                       # Not used with sqlite3.
+DATABASE_PASSWORD = ''                   # Not used with sqlite3.
+DATABASE_HOST = ''                       # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''                       # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -35,7 +35,8 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '' # In this installation, the realtive path will be the public_html directory below
+                # the current working directory. This remains unset until installed on web server.
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
