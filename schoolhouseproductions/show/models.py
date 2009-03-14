ad71b472df752fbf1c4287a026adcf489d74fd89
Kenley Cheung <ken@lordkenthegreat.com>
2009-03-14 16:24:49 -0400

    Created basic video and show models.  Code has NOT been tested yet.

--- schoolhouseproductions/show/models.py
+++ schoolhouseproductions/show/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
+
+class Show(models.Model):
+    name = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ['name', 'creation_date', 'update_time']
+        db_table = 'shows'
+        get_latest_by = "update_time"
+
+admin.site.register(Show)
--- schoolhouseproductions/video/models.py
+++ schoolhouseproductions/video/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib import admin
+# Special import of the shows model.
+
+from schoolhouseproductions.shows import Show
 
 # Create your models here.
+
+# Creating basic video model.
+
+class Video(models.Model)
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=1000)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    show = models.ForeignKey(Show)
+    video_url = models.URLField(max_length=300)
+
+admin.site.register(Video)
