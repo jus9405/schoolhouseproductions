4503971136a6a2134eaeadcb62f91960df112dc9
Kenley Cheung <ken@lordkenthegreat.com>
2009-03-15 09:31:57 -0400

    Creating view skeletons.

--- schoolhouseproductions/show/views.py
+++ schoolhouseproductions/show/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+
+
+def list_recently_updated_shows:
+
+def list_newest_shows:
+
+def list_recently_discussed_shows:
+
+def list_most_discussed_shows:
+
+def list_popular_shows:
--- schoolhouseproductions/video/views.py
+++ schoolhouseproductions/video/views.py
@@ -1,1 +1,15 @@
 # Create your views here.
+
+def list_recent_videos:
+
+def list_recently_commented_videos:
+
+def list_most_popular_videos:
+
+def list_most_discussed_videos:
+
+def get_video_comments:
+
+def post_video_comment:
+
+def get_video:
